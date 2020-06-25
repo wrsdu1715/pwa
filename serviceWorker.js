9bf3195bb3316256ce693a0fa35e1b9965b6caee
H-1911 <h-1911@H-1911.local>
2020-06-25 23:54:51 +0900

    fetch追加

--- serviceWorker.js
+++ serviceWorker.js
@@ -15,3 +15,13 @@ self.addEventListener('install', (event) => {
       })
   );
 });
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches
+      .match(event.request)
+      .then((response) => {
+        return response ? response : fetch(event.request);
+      })
+  );
+});
