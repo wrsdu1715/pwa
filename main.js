e864d414eba5986d0ed2a0bbefe14e0395ea882f
H-1911 <h-1911@H-1911.local>
2020-06-25 23:23:49 +0900

    main.jsの追加

---
+++ main.js
@@ -0,0 +1,8 @@
+if('serviceWorker' in navigator) {
+  navigator.serviceWorker.register('./serviceWorker', {scope: '/'})
+  .then(function(reg) {
+    console.log('登録に成功しました。 Scopeは' + reg.scope);
+  }).catch(function(error){
+    console.log('登録に失敗しました。' + error);
+  });
+}
