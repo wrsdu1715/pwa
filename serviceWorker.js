c62ea19fe699c9473406084779ba0b84faaaa755
H-1911 <h-1911@H-1911.local>
2020-06-27 22:47:24 +0900

    sw.jsのactive,fetchを更新

--- serviceWorker.js
+++ serviceWorker.js
@@ -1,12 +1,11 @@
 'use strict';
 
-const CACHE_NAME = 'pwa2'; //キャッシュさせる時のキャッシュ名
+const CACHE_NAME = 'pwa-v3'; //キャッシュさせる時のキャッシュ名
 const urlsToCache = [
   './',
   './index.html',
   './common.css',
   './main.js',
-  './manifest.json'
 ];
 
 self.addEventListener('install', (event) => { //install時
@@ -18,12 +17,19 @@ self.addEventListener('install', (event) => { //install時
   );
 });
 
+// キャッシュcaches.openで開いて、cache.addAllで保存させる。
+// event.waitUntilはPromiseをとって、インストールが成功仕方を確認するために使う。
+//  ┗https://developer.mozilla.org/ja/docs/Web/API/ExtendableEvent/waitUntil
+// ※ファイル数が多くなればなるほどキャッシュが失敗してService Workerがインストールされない確率も高くなる。
+
+
+
 self.addEventListener('active', (event) => {
   var cacheWhitelist = [CACHE_NAME];
 
   event.waitUntil(
     caches.keys().then((cacheNames) => {
-      return Promise.add(
+      return Promise.all(
         cacheNames.map((cacheName) => {  //ホワイトリストにないキャッシュは削除させる。
           if (cacheWhitelist.indexOf(cacheName) === -1) {
             return caches.delete(cacheName);
@@ -34,12 +40,30 @@ self.addEventListener('active', (event) => {
   );
 });
 
-self.addEventListener('fetch', (event) => {
+self.addEventListener('fetch', (event) => {　//ページの更新などで、Service Workerはfetchイベントを受け取る。
   event.respondWith(
-    caches
-      .match(event.request)
-      .then((response) => {
-        return response ? response : fetch(event.request);
-      })
+    caches.match(event.request) 
+     .then((response) => {　
+       if (response) {
+         return response;
+       }
+       
+       let fetchRequest = event.request.clone();
+       return fetch(fetchRequest)
+        .then((response) => {
+          if(!response || response.status !== 200 || response.type !== 'basic') {
+            //レスポンスが正しいか、レスポンスのステータスが200か、レスポンスの型がbasicか。（リクエストの送信元と送信先のドメインが同じである）
+            return response;
+          }
+
+          let responseToCache = response.clone();
+          cache.open(CACHE_NAME)
+            .then((cache) => {
+              cache.put(event.request, responseToCache);
+            });
+
+            return response;
+        });
+     })
   );
 });
